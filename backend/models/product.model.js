7041b2d753aad9ac07c2f682fa4c3628a655ff43
Tomas Kravcik <root@Laptop-i7>
2024-09-08 16:16:13 +0200

    Post req for Products

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -10,7 +10,7 @@ const productSchema = new mongoose.Schema({
             type: Number,
             required: true
         },
-        images: {
+        image: {
             type: String,
             required: true
         }
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
 import express from 'express';
 import {connectDB} from "./config/db.js";
+import Product from "./models/product.model.js";
 
 
 const app = express();
 
+app.use(express.json()) //MW allows to accept json data in body
 
-app.get('/products', (req, res) => {
+app.get('/api/products', (req, res) => {
     res.send("hello")
+})
+
+app.post('/api/products', async (req, res) => {
+    const product = req.body
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({success: false, message: "Some fields ware not provided"})
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+        res.status(200).json({success: true, message: "Product saved successfully", data: newProduct})
+    } catch (err) {
+        console.log("Error saving product" + err.message)
+        res.status(500).json({success: false, message: "Product not saved", error: err.message})
+    }
 })
 
 app.listen(5000, (err, res) => {
