3635141b29ecec7b562644720b26784dcc109ce1
Tomas Kravcik <root@Laptop-i7>
2024-09-08 16:33:21 +0200

    GEt, delete req for Products

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,25 @@ const app = express();
 
 app.use(express.json()) //MW allows to accept json data in body
 
-app.get('/api/products', (req, res) => {
-    res.send("hello")
+app.get('/api/products', async (req, res) => {
+
+    try {
+        const products = await Product.find({}) //finds all products
+        res.status(200).json({success: true, data: products})
+    } catch (err) {
+        res.status(500).json({success: false, message: err.message})
+    }
+})
+
+app.delete('/api/products/:id', async (req, res) => {
+    const {id} = req.params
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product deleted successfully"})
+    } catch (err) {
+        res.status(404).json({success: false, message: "Product not found"})
+    }
 })
 
 app.post('/api/products', async (req, res) => {
