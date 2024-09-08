5517d5984c477e843405d055f031c706ed4b9950
Tomas Kravcik <root@Laptop-i7>
2024-09-08 21:20:22 +0200

    prepare for prod deploy

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,26 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from "./config/db.js";
 import productRoute from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json()) //MW allows to accept json data in body
 
 app.use("/api/products", productRoute)
+
+if (process.env.NODE_ENV === 'production') {
+    //make dist folder (builded front)  as static assets
+    app.use(express.static(path.join(__dirname, "frontend/dist")))
+
+    //serve index.html
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 
 app.listen(port, (err, res) => {
