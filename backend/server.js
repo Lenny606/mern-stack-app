9e4487ad761b8696f23f96d90e0004391fd65290
Tomas Kravcik <root@Laptop-i7>
2024-09-08 16:45:59 +0200

    refactor reqs routes

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,14 @@
 import express from 'express';
 import {connectDB} from "./config/db.js";
-import Product from "./models/product.model.js";
-import mongoose from "mongoose";
+import productRoute from "./routes/product.route.js";
 
 
 const app = express();
 
 app.use(express.json()) //MW allows to accept json data in body
 
-app.get('/api/products', async (req, res) => {
+app.use("/api/products", productRoute)
 
-    try {
-        const products = await Product.find({}) //finds all products
-        res.status(200).json({success: true, data: products})
-    } catch (err) {
-        res.status(500).json({success: false, message: err.message})
-    }
-})
-
-app.delete('/api/products/:id', async (req, res) => {
-    const {id} = req.params
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product deleted successfully"})
-    } catch (err) {
-        res.status(404).json({success: false, message: "Product not found"})
-    }
-})
-
-app.put('/api/products/:id', async (req, res) => {
-    const {id} = req.params
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({success: false, message: "Id invalid"})
-    }
-
-    const product = req.body
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
-        res.status(200).json({success: true, message: "Product updated successfully", data: updatedProduct})
-    } catch (err) {
-        res.status(500).json({success: false, message: err.message})
-    }
-})
-
-app.post('/api/products', async (req, res) => {
-    const product = req.body
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({success: false, message: "Some fields ware not provided"})
-    }
-
-    const newProduct = new Product(product)
-
-    try {
-        await newProduct.save()
-        res.status(200).json({success: true, message: "Product saved successfully", data: newProduct})
-    } catch (err) {
-        console.log("Error saving product" + err.message)
-        res.status(500).json({success: false, message: "Product not saved", error: err.message})
-    }
-})
 
 app.listen(5000, (err, res) => {
     connectDB();
