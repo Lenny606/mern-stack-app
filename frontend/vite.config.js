5c1706db27222347da4ca6971f49a5e4605754d4
Tomas Kravcik <root@Laptop-i7>
2024-09-08 22:09:32 +0200

    vit config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000"
+        target: "https://mern-stack-app-sjj5.onrender.com/"
       }
     }
   }
