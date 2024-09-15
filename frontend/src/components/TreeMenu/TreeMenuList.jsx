6bf8e32087674a758414560d7492b9bbfd6bb9c1
Tomas Kravcik <root@Laptop-i7>
2024-09-15 12:37:17 +0200

    some shitty hamburger menu

--- frontend/src/components/TreeMenu/TreeMenuList.jsx
+++ frontend/src/components/TreeMenu/TreeMenuList.jsx
@@ -1,17 +1,34 @@
 import TreeMenuItem from "./TreeMenuItem.jsx";
+import {useState} from "react";
 
-export default function TreeMenuList({list = [], isChild = false}) {
+export default function TreeMenuList({ list = [], isChild = false }) {
+    const [isOpen, setIsOpen] = useState(false); // State to toggle the menu
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen); // Toggle the menu state
+    };
 
     return (
-        <ul className={"menu-list-container"}>
-            {/*list check*/}
-            {
-                list && list.length
-                    ? list.map((listItem) => {
-                        return <TreeMenuItem key={listItem.id} item={listItem} isChild={isChild}/>
-                    })
-                    : null
-            }
-        </ul>
-    )
+        <div className="menu-wrapper">
+            {/* Hamburger menu button */}
+            {!isChild && (
+                <div className="hamburger" onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            )}
+
+            {/* Menu List */}
+            <ul className={`menu-list-container ${isOpen ? 'active' : 'hidden'}`}>
+                {
+                    list && list.length
+                        ? list.map((listItem) => {
+                            return <TreeMenuItem key={listItem.id} item={listItem} isChild={isChild} />;
+                        })
+                        : null
+                }
+            </ul>
+        </div>
+    );
 }
