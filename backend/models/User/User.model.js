0ea44bca1613c07e031f7e9388ff0a57f83c2b1a
Tomas Kravcik <root@Laptop-i7>
2024-09-15 15:32:29 +0200

    reafactor User

--- backend/models/User/User.model.js
+++ backend/models/User/User.model.js
@@ -1,28 +1,95 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema(
-    {
-        name: String,
-        profilePic: String,
-        email: String,
-        password: String,
-        birthDate: Date,
-        accountActivated: {
-            type: Boolean,
-            default: false
-        },
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+        minlength: [2, 'Name must be at least 2 characters long'],
+        maxlength: [50, 'Name cannot exceed 50 characters']
     },
-    {
-        timestamps: true,
-        discriminatorKey: "role",
-    }
-);
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: function(v) {
+                return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email address!`
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [8, 'Password must be at least 8 characters long'],
+        select: false
+    },
+    profilePic: {
+        type: String,
+        default: 'default-profile.jpg'
+    },
+    birthDate: {
+        type: Date,
+        validate: {
+            validator: function(v) {
+                return v && v.getTime() < Date.now();
+            },
+            message: 'Birth date must be in the past'
+        }
+    },
+    phoneNumber: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /\d{10}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    },
+    address: {
+        street: String,
+        city: String,
+        state: String,
+        country: String,
+        zipCode: String
+    },
+    accountActivated: {
+        type: Boolean,
+        default: false
+    },
+    lastLogin: {
+        type: Date
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
+}, {
+    timestamps: true,
+    discriminatorKey: "role"
+});
+
+// Pre-save hook to hash password
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+// Method to check if password is correct
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 //Add the methods
 userSchema.statics.findByEmail = async function (email) {
     return await this.findOne({email})
 }
-userSchema.statics.countUsers = function () {
-    return this.countDocuments({});
+userSchema.statics.countUsers = async function () {
+    return await this.countDocuments({});
 };
 
 const User = mongoose.model("User", userSchema);
