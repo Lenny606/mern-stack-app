981e2ca7ff513221023738c8e7a82a3f541f5cfd
Tomas Kravcik <root@Laptop-i7>
2024-09-22 09:56:45 +0200

    add get route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {createUser} from "../controllers/user.controller.js";
+import {createUser, getUsers} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post('/', createUser);
+router.get('/', getUsers);
 // router.delete('/:id', deleteProduct)
 // router.put('/:id', updateProduct)
 // router.post('/', createProduct)
